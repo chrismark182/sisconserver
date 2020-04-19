c37f087a29f16643dc0fb16bcb76ca7ff9c249d4
WillyJm <36754544+WillyJm@users.noreply.github.com>
2020-04-19 17:45:23 -0500

    agregando lista de ubicacion

--- application/views/cliente/V_editar.php
+++ application/views/cliente/V_editar.php
@@ -1,3 +1,12 @@
+<nav class="blue-grey lighten-1" style="padding: 0 1em;">
+    <div class="nav-wrapper">
+      <div class="col s12">
+        <a href="#!" class="breadcrumb">Mantenimientos</a>
+        <a href="#!" class="breadcrumb">Clientes</a>
+        <a href="#!" class="breadcrumb">Editar</a>
+      </div>
+    </div>
+</nav>
 <div class="section container center">
     <form action="<?= base_url() ?>cliente/<?= $cliente->EMPRES_N_ID ?>/<?= $cliente->CLIENT_N_ID ?>/actualizar" method="post">
         <div class="row">
--- application/views/ubicacion/V_editar.php
+++ application/views/ubicacion/V_editar.php
@@ -1,3 +1,12 @@
+<nav class="blue-grey lighten-1" style="padding: 0 1em;">
+    <div class="nav-wrapper">
+      <div class="col s12">
+        <a href="#!" class="breadcrumb">Mantenimientos</a>
+        <a href="#!" class="breadcrumb">Ubicaciones</a>
+        <a href="#!" class="breadcrumb">Editar</a>
+      </div>
+    </div>
+</nav>
 <div class="section container center">
     <form action="<?= base_url() ?>ubicacion/<?= $ubicacion->EMPRES_N_ID ?>/<?= $ubicacion->SEDE_N_ID ?>/<?= $ubicacion->UBICAC_N_ID ?>/actualizar" method="post">
         <div class="row">
