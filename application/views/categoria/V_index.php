4e113bb18c388aa76a95b8b0231abba01796b466
tfbm89 <tfbm89@gmail.com>
2020-06-23 10:48:01 -0500

    se creo un espacio en el index
    espacio en el index

--- application/views/categoria/V_index.php
+++ application/views/categoria/V_index.php
@@ -1,8 +1,8 @@
-<nav class="blue-grey lighten-1" style="padding: 0 1em;">
+<nav  class="blue-grey lighten-1" style="padding: 0 1em;">
     <div class="nav-wrapper">
       <div class="col s12">
-        <a href="#!" class="breadcrumb">Seguridad</a>
-        <a href="#!" class="breadcrumb">Categorias</a>
+        <a href="#!" class="breadcrumb">Seguridad </a>
+        <a href="#!" class="breadcrumb">Categorias </a>
       </div>
     </div>
 </nav>
