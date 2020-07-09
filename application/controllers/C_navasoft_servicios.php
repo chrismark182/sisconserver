fbd69b79677fb10f82d46029b29447f018386967
CesarSoriano <csoriano@logisticaoquendo.com>
2020-07-09 00:09:16 -0500

    Archivos DBF para navasoft

--- application/controllers/C_navasoft_servicios.php
+++ application/controllers/C_navasoft_servicios.php
@@ -12,15 +12,17 @@ class C_navasoft_servicios extends CI_Controller {
 			$this->data['session'] = $this->esandexaccesos->session();
             $this->data['accesos'] = $this->esandexaccesos->accesos();
             $empresa = $this->M_crud->read('empresa', array('EMPRES_N_ID' => $this->session->userdata('empresa_id')));
-            $this->data['empresa']=$empresa[0];      
+            $this->data['empresa']=$empresa[0];     
 		else:
 			redirect(base_url(),'refresh');
 		endif;
     }
+
     private function _init()
 	{
 		$this->output->set_template('siscon');
-	}
+    }
+    
     //Vistas
     public function index() 
 	{         
@@ -47,64 +49,34 @@ class C_navasoft_servicios extends CI_Controller {
     public function generar_dbf()
     {
         $data = json_decode(file_get_contents('php://input'), true);
-        // $sql = "Exec LIQUIDACIONES_LIS_PARANAVASOFT 1, {$data['empresa']}, {$data['liquidacion']}, {$data['usuario']}";
-
-        // $resultSet = $this->M_crud->sql($sql);
+        $sql = "Exec LIQUIDACIONES_LIS_PARANAVASOFT 1, {$data['empresa']}, {$data['liquidacion']}, {$data['usuario']}";
+        $resultSet = $this->M_crud->sql($sql);
         
-        // $cabecera = array(
-        //                 'numerodocu'    => $resultSet[0]['NUMERODOCU'], 
-        //                 'guiahija'      => $resultSet[0]['GUIAHIJA'], 
-        //                 'peso'          => $resultSet[0]['PESO'],
-        //                 'bultos'        => $resultSet[0]['BULTOS'],
-        //                 'manifiesto'    => $resultSet[0]['MANIFIESTO'],
-        //                 'numseriado'    => $resultSet[0]['NUMSERIADO'],
-        //                 'idtipodocu'    => $resultSet[0]['IDTIPODOCU'],
-        //                 'fecemision'    => $resultSet[0]['FECEMISION'],
-        //                 'idmoneda'      => $resultSet[0]['IDMONEDA'],
-        //                 'idcliente'     => $resultSet[0]['IDCLIENTE'],
-        //                 'idaduana'      => $resultSet[0]['IDADUANA'],
-        //                 'numcheque'     => $resultSet[0]['NUMCHEQUE'],
-        //                 'idbancoseg'    => $resultSet[0]['IDBANCOSEG'],
-        //                 'conversion'    => $resultSet[0]['CONVERSION'],
-        //                 'montoafect'    => $resultSet[0]['MONTOAFECT'],
-        //                 'montoexone'    => $resultSet[0]['MONTOEXONE'],
-        //                 'montoigv'      => $resultSet[0]['MONTOIGV'],
-        //                 'total'         => $resultSet[0]['TOTAL'],
-        //                 'usuario'       => $resultSet[0]['USUARIO'],
-        //                 'status'        => $resultSet[0]['STATUSS'],
-        //                 'flag'          => $resultSet[0]['FLAG'],
-        //                 'fectra'        => $resultSet[0]['FECTRA'],
-        //                 'iddespacha'    => $resultSet[0]['IDDESPACHA'],
-        //                 'tipocambio'    => $resultSet[0]['TIPOCAMBIO'],
-        //                 'guiaremisi'    => $resultSet[0]['GUIAREMISI'],
-        //                 'observacio'    => $resultSet[0]['OBSERVACIO'],
-        //                 'numdoc'        => $resultSet[0]['NUMDOC'],
-        //                 'seriedoc'      => $resultSet[0]['SERIEDOC'],
-        //                 'fechadoc'      => $resultSet[0]['FECHADOC'],
-        //                 'impexp'        => $resultSet[0]['IMPEXP'],
-        //                 'facespecie'    => $resultSet[0]['FACESPECIE'],
-        //                 'contenido'     => $resultSet[0]['CONTENIDO'],
-        //                 'idtipdocnc'    => $resultSet[0]['IDT'],
-        //                 'facturadoa'    => $resultSet[0]['FACTURADOA'],
-        //                 'tipopago'      => $resultSet[0]['TIPOPAGO'],
-        //                 'idagenciac'    => $resultSet[0]['IDAGENCIAC'],
-        //                 'feccalculo'    => $resultSet[0]['FECCALCULO'],
-        //                 'diasestadi'    => $resultSet[0]['DIASESTADI'],
-        //                 'numvolante'    => $resultSet[0]['NUMVOLANTE'],
-        //                 'posicion'      => $resultSet[0]['POSICION'],
-        //                 'dua'           => $resultSet[0]['DUA'],
-        //                 'fecllegada'    => $resultSet[0]['FECLLEGADA'],
-        //                 'nomdespach'    => $resultSet[0]['NOMDESPACH'],
-        //                 'codd'          => $resultSet[0]['CODD'],
-        //                 'nrod'          => $resultSet[0]['NROD'],
-        //                 'origen'        => $resultSet[0]['ORIGEN']
-        //             );
-        // $result = (object) null;
+        $cabecera = array($sql, $resultSet);
+        $result = (object) null;
         
+        ////$this->m_dbf->create('docterminal', $cabecera, 0);
+
+        $rutaLocal = realpath(__DIR__ . '/..') . '\\dbf\\generado\\';
+        //$rutaServer = '\\\\10.0.0.22\\DataCarga\\DBFCOURIER\\';
+        $rutaServer = 'D:\\DataCarga\\';
+
+        $dbfCabecera = 'docterminal.dbf';
+        $dbfDetalle = 'detdocterminal.dbf';
         
-        // $this->m_dbf->create('docterminal', $cabecera, 0);
+        error_reporting(E_ERROR | E_PARSE);
 
-
+        $result->status=0;
+        if(count($cabecera)>0):
+            $result->status=1;
+            if(!copy($rutaLocal . $dbfCabecera, $rutaServer . $dbfCabecera)):
+                $result->errorCabecera = "Error al copiar ". $dbfCabecera;
+            endif;
+            if(!copy($rutaLocal . $dbfDetalle, $rutaServer . $dbfDetalle)):
+                $result->errorDetalle = "Error al copiar " . $dbfDetalle;
+            endif;
+            $result->data=$cabecera;       
+        endif;
 
         $sql= "Exec LIQUIDACION_UPD_NAVASOFT {$data['empresa']}, {$data['liquidacion']}, {$data['usuario']}";
         $query = $this->M_crud->sql($sql);
