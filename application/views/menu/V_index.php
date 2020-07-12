51270c3508060814e950e7491f08d56dc8509d58
CesarSoriano <csoriano@logisticaoquendo.com>
2020-07-12 17:50:51 -0500

    Ajustes en Opciones del Sistema

--- application/views/menu/V_index.php
+++ application/views/menu/V_index.php
@@ -1,34 +1,51 @@
 <nav class="blue-grey lighten-1" style="padding: 0 1em;">
     <div class="nav-wrapper">
-      <div class="col s12">
-        <a href="#!" class="breadcrumb">Mantenimientos</a>
-        <a href="#!" class="breadcrumb">Menus</a>
-      </div>
+        <div class="col s4" style="display: inline-block">
+            <a href="#!" class="breadcrumb">Seguridad</a>
+            <a href="#!" class="breadcrumb">Opciones del Sistema</a>
+        </div>
+        <ul id="nav-mobile" class="right">
+            <div class="input-field col s6 left-align" style="margin: 0px; font-size: 12px;">
+                <div>
+                    <b>
+                        Total Registros: 
+                        &nbsp;&nbsp;&nbsp;
+                        <span id="total" class="btn blue-grey darken-2"><?php echo count($menus);?></span>
+                    </b>
+                </div>
+            </div>
+        </ul>
     </div>
 </nav>
+
 <div class="section container">
-    <table>
-        <thead>
+    <div>
+        &nbsp;
+    </div>
+
+    <table class="striped" style="font-size: 12px;">
+        <thead class="blue-grey darken-1" style="color: white">
             <tr>     
-                <th>Menú Padre</th>
-                <th>Decripción</th>
-                <th>Ruta</th>
-                <th>Editar</th>
+                <th class="left-align">MENÚ PRINCIPAL</th>
+                <th class="left-align">OPCIÓN DEL MENÚ</th>
+                <th class="left-align">RUTA</th>
+                <th class="center-align">EDITAR</th>
+                <th class="center-align">ELIMINAR</th>
             </tr>
         </thead>
         <tbody>
             <?php if($menus): ?>
                 <?php foreach($menus as $menu): ?> 
                     <tr>
-                        <td><?=$menu->MENU_PADRE_DESCRIPCION?></td>
-                        <td><?=$menu->MENU_DESCRIPCION?></td>
-                        <td><a href="<?= base_url() ?><?= $menu->MENU_RUTA ?>"><?= base_url() ?><?= $menu->MENU_RUTA ?></a></td>
-                        <td>
+                        <td class="left-align"><?=$menu->MENU_PADRE_DESCRIPCION?></td>
+                        <td class="left-align"><?=$menu->MENU_DESCRIPCION?></td>
+                        <td class="left-align"><a href="<?= base_url() ?><?= $menu->MENU_RUTA ?>"><?= base_url() ?><?= $menu->MENU_RUTA ?></a></td>
+                        <td class="center-align">
                             <a href="<?= base_url() ?>menu/<?= $menu->MENU_ID ?>/editar">
                                 <i class="material-icons">edit</i>
                             </a>
                         </td>
-                        <td>
+                        <td class="center-align">
                             <a href="<?= base_url() ?>menu/<?= $menu->MENU_ID ?>/eliminar")>
                                 <i class="material-icons">delete</i>
                             </a>
@@ -39,5 +56,6 @@
         </tbody>
     </table>
 </div>
+
 <a  class="btn-floating btn-large waves-effect waves-light red" style="bottom:16px; right:16px; position:fixed;" 
     href="<?= base_url()?>menu/nuevo"><i class="material-icons">add</i></a>
--- application/views/menu/V_nuevo.php
+++ application/views/menu/V_nuevo.php
@@ -1,8 +1,8 @@
 <nav class="blue-grey lighten-1" style="padding: 0 1em;">
     <div class="nav-wrapper">
       <div class="col s12">
-        <a href="#!" class="breadcrumb">Mantenimientos</a>
-        <a href="#!" class="breadcrumb">Menus</a>
+        <a href="#!" class="breadcrumb">Seguridad</a>
+        <a href="<?= base_url() ?>menus" class="breadcrumb">Opciones del Sistema</a>
         <a href="#!" class="breadcrumb">Nuevo</a>
       </div>
     </div>
@@ -18,18 +18,18 @@
                     <?php endforeach; ?>
 
                 </select>
-                <label>Menú padre</label>
+                <label>Menú Padre</label>
             </div>
             <div class="input-field col s12 m6 l4">
-                <input id="descripcion" type="text" name="descripcion" class="validate">
-                <label class="active" for="descripcion">Descripción</label> 
+                <input id="descripcion" maxlength="50" type="text" name="descripcion" class="validate">
+                <label class="active" for="descripcion">Opción del menú</label> 
             </div>
             <div class="input-field col s12 m6 l4">
-                <input id="ruta" type="text" name="ruta" class="validate">
+                <input id="ruta" maxlength="50" type="text" name="ruta" class="validate">
                 <label class="active" for="ruta">Ruta</label> 
             </div>
             <div class="input-field col s12">
-                <input class="btn-large" type="submit" value="Guardar">
+                <input class="btn-small" type="submit" value="Guardar">
             </div>
         </div>
     </form>
