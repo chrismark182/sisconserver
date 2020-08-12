04f6cf3b94918a714b71c6b1df527c9f74bf2547
burngeek8 <burngeek8@gmail.com>
2020-08-12 01:36:54 -0500

    recepcion de documento listado y upload file

--- application/views/recepcion_doc/V_index.php
+++ application/views/recepcion_doc/V_index.php
@@ -1,3 +1,7 @@
+<?php 
+    $fechaDesde = new DateTime();
+    $fechaHasta = new DateTime();
+?>
 <nav class="blue-grey lighten-1" style="padding: 0 1em;">
     <div class="nav-wrapper">
         <div class="col s4" style="display: inline-block">
@@ -20,26 +24,23 @@
 <div class="section container center" style="padding-top: 0px">
     <div class="row" style="margin-bottom: 0px">
         <form action="<?= base_url() ?>clientes" method="post">
-          
-            <div class="input-field col s12 m6 l4">
-                <input id="nombre" maxlength="200" type="text" name="nombre"  class="validate">
-                <label class="active" for="nombre">Nombre</label> 
-            </div>
-
 			<div class="input-field col s12 m6 l4">
-                <input id="apellido" maxlength="200" type="text" name="apellido"  class="validate">
-                <label class="active" for="apellido">Apellido</label> 
+                <input id="desde" type="text" value="<?= $fechaDesde->format('m/d/Y') ?>" class="datepicker">
+                <label class="active" for="desde">Desde</label> 
             </div>
-
-			<div class="input-field col s12 m6 l4">
-                <input id="n_documento" maxlength="11" type="text" class="validate">
-                <label class="active" for="n_documento">Numero de documento</label> 
-            </div>	
-
             <div class="input-field col s12 m6 l4">
-                <input id="nombre_empresa" maxlength="100" type="text"  class="validate">
-                <label class="active" for="nombre_empresa">Nombre de empresa</label> 
+                <input id="hasta" type="text" value="<?= $fechaHasta->format('m/d/Y') ?>" class="datepicker">
+                <label class="active" for="hasta">Hasta</label> 
             </div>
+            <div class="input-field col s12 m6 l4">
+                <input id="empresa_de" maxlength="200" type="text">
+                <label class="active" for="empresa_de">Empresa de</label> 
+            </div>
+			<div class="input-field col s12 m6 l4">
+                <input id="empresa_para" maxlength="200" type="text">
+                <label class="active" for="empresa_de">Empresa para</label> 
+            </div>
+			
             <div class="input-field col s4">
                 <div class="btn-small" id="btnBuscar" onclick="buscar()" >Buscar</div>
             </div>
@@ -51,13 +52,14 @@
     <table class="striped" style="font-size: 12px;">
         <thead class="blue-grey darken-1" style="color: white">
             <tr>          
-                <th class="left-align">Nombre </th>
-                <th class="left-align">Apellido</th>
-                <th class="left-align">N Documento</th>
-				<th class="left-align">Empresa</th>
-				<th class="left-align" style="text-align: center">Info Bloqueo</th>
-				<th class="left-align">Usuario</th>
-				<th class="left-align">Fecha Bloqueo</th>          
+                <th class="left-align">ID </th>
+                <th class="left-align">Fecha de recepción</th>
+                <th class="left-align">Empresa De</th>
+				<th class="left-align">Empresa Para</th>
+				<th class="left-align">Tipo de documento</th>
+				<th class="left-align">Número documento</th>
+				<th class="center-align">Escaneo Adjunto</th>
+				<th class="left-align">Fecha Vencimiento</th>          
             </tr>
         </thead>
         <tbody id="resultados">   
@@ -69,56 +71,27 @@
 
 <!-- Modal Structure -->
 
-<div id="modalInfoBloqueos" class="modal modal-fixed-footer">
+<div id="modalUpload" class="modal modal-fixed-footer">
     <div class="modal-content" >
-		<h4>Datos del bloqueo</h4>
-		<div class="divider"></div>
-		<p>Motivo bloqueo: <span id="bloqueo_motivo"></span></p>
-		<p>Usuario bloqueo: <span id="bloqueo_usuario"></span></p>
-		<p>Fecha bloqueo: <span id="bloqueo_fecha"></span></p>
-		<div class="btn" onclick="modalDesbloquear()">DESBLOQUEAR</div>
+		<h4>Adjuntar archivo</h4>
+		<div class="row">
+			<input type="hidden" id="id_movimiento">
+			<input type="hidden" id="name_file">
+			<div class="file-field input-field">
+				<div class="btn">
+					<span>Archivo</span>
+					<input id="archivo" type="file">
+				</div>
+				<div class="file-path-wrapper">
+					<input class="file-path validate" type="text">
+				</div>
+			</div>
+		</div>
     </div>
 	<div class="modal-footer">
-      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Aceptar</a>
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat" onclick="validarUpload()">Aceptar</a>
     </div>
 </div> 
-
-<div id="modalInfoDesbloqueado" class="modal modal-fixed-footer">
-    <div class="modal-content" >
-		<h4>Datos del bloqueo</h4>
-		<div class="divider"></div>
-		<p>Motivo bloqueo: <span id="des_bloqueo_motivo"></span></p>
-		<p>Usuario bloqueo: <span id="des_bloqueo_usuario"></span></p>
-		<p>Fecha bloqueo: <span id="des_bloqueo_fecha"></span></p>
-		<br>
-		<h4>Datos del desbloqueo</h4>
-		<div class="divider"></div>
-		<p>Motivo desbloqueo: <span id="desbloqueo_motivo"></span></p>
-		<p>Usuario desbloqueo: <span id="desbloqueo_usuario"></span></p>
-		<p>Fecha desbloqueo: <span id="desbloqueo_fecha"></span></p>
-		
-    </div>
-	<div class="modal-footer">
-      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Aceptar</a>
-    </div>
-</div> 
-
-<div id="modalBloqueos" class="modal modal-fixed-footer">
-    <div class="modal-content">
-        <h4 class="left">Desbloquear persona</h4>
-        <div class="section row">
-            <input type="hidden" id="persona_id" >
-			<input type="hidden" id="bloqueo_item" >
-            <div class="input-field col s12">
-                <textarea id="motivo" class="materialize-textarea"></textarea>
-                <label for="motivo">Motivo del desbloqueo</label>
-            </div>
-        </div>
-    </div>
-    <div class="modal-footer">
-        <a href="#!" class=" waves-effect waves-green btn-flat" onclick="desbloquear()">Aceptar</a>
-    </div>
-</div>
 
 <script>
 	document.addEventListener('DOMContentLoaded', function() {
@@ -129,31 +102,32 @@
 			M.updateTextFields();
 			buscar()
 		}
+		buscar()
 	});
 	function buscar(){
 
 		$('.preloader-background').css({'display': 'block'});
 		let url = '<?= base_url() ?>api/execsp';
 
-		let sp = 'PERSONA_BLOQUEO_LIS';
+		let sp = 'MOVIMIENTO_DOCUMENTO_LIS';
 		
-		let nombre = '%';
-		if(document.getElementById('nombre').value != ''){
-			nombre = document.getElementById('nombre').value + '%';
+		let fecha_desde = $('#desde').val();
+			fecha_desde = fecha_desde.split('/');
+		let desde = fecha_desde[2] + fecha_desde[1] + fecha_desde[0];
+		
+		let fecha_hasta = $('#hasta').val();
+			fecha_hasta = fecha_hasta.split('/');
+		let hasta = fecha_hasta[2] + fecha_hasta[1] + fecha_hasta[0];
+
+		let empresa_de = '%';
+		if(document.getElementById('empresa_de').value != ''){
+			empresa_de = document.getElementById('empresa_de').value + '%';
 		}
-		let apellido = '%';
-		if(document.getElementById('apellido').value != ''){
-			apellido = document.getElementById('apellido').value + '%';
+		let empresa_para = '%';
+		if(document.getElementById('empresa_para').value != ''){
+			empresa_para = document.getElementById('empresa_para').value + '%';
 		}
-		let numero_documento = '%';
-		if(document.getElementById('n_documento').value != ''){
-			numero_documento = document.getElementById('n_documento').value + '%';
-		}
-		let nombre_empresa = '%';
-		if(document.getElementById('nombre_empresa').value != ''){
-			nombre_empresa = document.getElementById('nombre_empresa').value + '%';
-		}
-        data = {sp, nombre, apellido, numero_documento, nombre_empresa };
+		let data = {sp, desde, hasta, empresa_de, empresa_para};
         
         $('#resultados').html('');
         fetch(url, {
@@ -174,20 +148,21 @@
 			{
 				for (let index = 0; index < data.length; index++) {
 					const element = data[index]; 
-					let bloqueo = `<i id="id_perbloqueo" class="material-icons  tooltipped" style="color: #039be5; cursor: pointer" data-tooltip="" onclick="muestraInfoBloqueo(${element.PERSON_N_ID},${element.PERBLO_N_ITEM})" >lock</i>`;
-					if(element.PERBLO_C_BLOQUEADO == '0')
+					let adjunto = `<i class="material-icons tooltipped" style="color: #039be5; cursor: pointer" onclick="modalUpload(${element.MOVDOC_N_ID})">attach_file</i>`;
+					if(element.MOVDOC_C_SITUACION == '1')
 					{
-						bloqueo = `<i id="id_perbloqueo" class="material-icons  tooltipped" style="color: #039be5; cursor: pointer" data-tooltip="" onclick="muestraInfoDesbloqueo(${element.PERSON_N_ID},${element.PERBLO_N_ITEM})">lock_open</i>`;
+						adjunto = `<a href="<?= base_url() ?>uploads/${element.MOVDOC_C_FOTO}" target="_blank"><i class="material-icons tooltipped" style="color: #039be5; cursor: pointer">attachment</i></a>`;
 					}
 					$('#resultados').append(`   		
 							<tr>
-								<td class="left-align">${element.PERSON_C_NOMBRE}</td>
-								<td class="left-align">${element.PERSON_C_APELLIDOS}</td>
-								<td class="left-align">${element.PERSON_C_DOCUMENTO}</td>
-								<td class="left-align">${element.CLIENT_C_RAZON_SOCIAL}</td>
-								<td style="text-align:center">${bloqueo}</td> 
-								<td class="left-align">${element.USUARI_C_USERNAME}</td>
-								<td class="left-align">${element.PERBLO_D_FECHA_REG}</td>
+								<td class="left-align">${element.MOVDOC_N_ID}</td>
+								<td class="left-align">${element.MOVDOC_C_FECHA_RECEPCION}</td>
+								<td class="left-align">${element.RAZON_SOCIAL_DE}</td>
+								<td class="left-align">${element.RAZON_SOCIAL_PARA}</td>
+								<td class="left-align">${element.TIDORE_C_ABREVIATURA}</td>
+								<td class="left-align">${element.MOVDOC_C_NUMERO_DOCUMENTO}</td>
+								<td class="center-align">${adjunto}</td>
+								<td class="left-align">${element.MOVDOC_D_FECHA_VENCIMIENTO}</td>
 							</tr>
 					`);
 				}
@@ -199,15 +174,26 @@
         });
 	}
 
-	function muestraInfoBloqueo(id, item)
+	function modalUpload(id)
 	{		
-		document.getElementById('persona_id').value = id;
-		document.getElementById('bloqueo_item').value = item;
-
+		document.getElementById('id_movimiento').value = id;
+		$('#modalUpload').modal('open');
+	}
+	async function validarUpload()
+	{
+		await uploadFile('archivo')
+		update();
+	}
+	function update()
+	{		
+		console.log('actualizando registro')
 		let url = '<?= base_url() ?>api/execsp';
-		let sp = 'PERSONA_BLOQUEO_ITEM_LIS';				
+		let sp = 'MOVIMIENTO_DOCUMENTO_UPD';				
 		let empresa = <?= $empresa->EMPRES_N_ID ?>;
-		data = {sp, empresa, id, item};
+		let id = document.getElementById('id_movimiento').value;
+		let nameFile = document.getElementById("name_file").value;
+		let usuario = <?= $session->USUARI_N_ID ?>; 
+		let data = {sp, empresa, id, nameFile, usuario};
 
 		fetch(url, {
 					method: 'POST', // or 'PUT'
@@ -222,86 +208,11 @@
 		.then(function(data) 
 		{
 			console.log(data);
-			let element = data[0];
-			document.getElementById('bloqueo_motivo').innerHTML = element.PERBLO_C_MOTIVO_BLOQUEO;
-			document.getElementById('bloqueo_usuario').innerHTML = element.USUARI_C_USERNAME;
-			document.getElementById('bloqueo_fecha').innerHTML = element.PERBLO_D_FECHA_REG;
-			$('#modalInfoBloqueos').modal('open');
+			buscar()
+
 			$('.preloader-background').css({'display': 'none'});                            
 		});
 	}
-	function muestraInfoDesbloqueo(id, item)
-	{		
-		document.getElementById('persona_id').value = id;
-		document.getElementById('bloqueo_item').value = item;
-
-		let url = '<?= base_url() ?>api/execsp';
-		let sp = 'PERSONA_BLOQUEO_ITEM_LIS';				
-		let empresa = <?= $empresa->EMPRES_N_ID ?>;
-		data = {sp, empresa, id, item};
-
-		fetch(url, {
-					method: 'POST', // or 'PUT'
-					body: JSON.stringify(data), // data can be `string` or {object}!
-					headers:{
-						'Content-Type': 'application/json'
-						}
-					})
-		.then(function(response) {
-			return response.json();
-		})
-		.then(function(data) 
-		{
-			console.log(data);
-			let element = data[0];
-			document.getElementById('des_bloqueo_motivo').innerHTML = element.PERBLO_C_MOTIVO_BLOQUEO;
-			document.getElementById('des_bloqueo_usuario').innerHTML = element.USUARI_C_USERNAME;
-			document.getElementById('des_bloqueo_fecha').innerHTML = element.PERBLO_D_FECHA_REG;
-
-			document.getElementById('desbloqueo_motivo').innerHTML = element.PERBLO_C_MOTIVO_DESBLOQUEO;
-			document.getElementById('desbloqueo_usuario').innerHTML = element.USUARIO_DESBLOQUEA;
-			document.getElementById('desbloqueo_fecha').innerHTML = element.PERBLO_D_FECHA_UPD;
-
-			$('#modalInfoDesbloqueado').modal('open');
-			$('.preloader-background').css({'display': 'none'});                            
-		});
-	}
-	function modalDesbloquear()
-    {
-		$('.modal').modal('close');
-        $('#modalBloqueos').modal('open');
-    }
-	function desbloquear()
-    {
-
-		let url = '<?= base_url() ?>api/execsp';
-		let sp = 'PERSONA_BLOQUEO_UPD';				
-		let empresa = <?= $empresa->EMPRES_N_ID ?>;
-		let persona = parseInt(document.getElementById('persona_id').value);
-		let item = parseInt(document.getElementById('bloqueo_item').value);
-		let motivo = document.getElementById('motivo').value;
-		let usuario = <?= $session->USUARI_N_ID ?>;
-		data = {sp, empresa, persona, item, motivo, usuario};
-
-		fetch(url, {
-                    method: 'POST', // or 'PUT'
-                    body: JSON.stringify(data), // data can be `string` or {object}!
-                    headers:{
-                        'Content-Type': 'application/json'
-                        }
-                    })
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) 
-        {
-			M.toast({html: 'Persona desbloqueada correctamente!', classes: 'rounded'});
-            $('.preloader-background').css({'display': 'none'});                            
-			setTimeout(() => {
-			    window.location.href= "<?= base_url() ?>bloqueos?n=" + data[0].PERSON_C_DOCUMENTO;                
-            }, 1000);
-		}).catch(error => console.log(error));
-		
-
-    }
+	
 </script>
+
